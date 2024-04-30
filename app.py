f819a9288b54eee1e1aca08858e39cf78e31773c
samuelndegwakiarie <sammiekiarie90@gmail.com>
2024-04-30 17:35:34 +0300

    added model added the 9 features in app.py

--- app.py
+++ app.py
@@ -1,44 +1,47 @@
 from flask import Flask, request, jsonify, render_template
 import joblib
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
 
-# Load your trained model
-model = joblib.load('/home/dynamic_dude/Genesis/water_quality/model.pkl')
+# Load model
+model = joblib.load('model/model.pkl')
 
-# Route for homepage
+# Load dataset
+df = pd.read_csv('/home/dynamic_dude/Genesis/water_quality/model/water_potability.csv')
+
+# Assuming 'Potability' is your target variable
+X = df.drop('Potability', axis=1)
+y = df['Potability']
+
+# Initialize StandardScaler
+scaler = StandardScaler()
+scaler.fit(X)
+
 @app.route('/')
 def index():
-    return render_template('index.html')  # Render the HTML template
+    return render_template('index.html')  
 
 # Route for making predictions
 @app.route('/predict', methods=['POST'])
 def predict():
-    try:
-        # Handle form submission (if applicable)
-        if request.method == 'POST':
-            # Extract feature values from form data
-            feature1 = request.form.get('feature1')
-            feature2 = request.form.get('feature2')
+    try:       
+        features = []  # Initialize the features list
+        for i in range(1, 10):
+            feature = request.form.get(f'feature{i}')
+            if feature is not None:  # Check if the value is not None
+                features.append(float(feature))
+            else:
+                # Handle the case where the value is None (e.g., provide a default value or skip)
+                pass
+            
+        scaled_features = scaler.transform([features])
 
-            # Convert feature values to float (assuming numerical features)
-            feature1 = float(feature1)
-            feature2 = float(feature2)
-        else:
-            # Handle JSON data (if applicable)
-            data = request.json
-            # Extract feature values from JSON data
-            feature1 = data.get('feature1')
-            feature2 = data.get('feature2')
+        # Perform prediction using loaded model
+        prediction = model.predict(scaled_features)
 
-            # Convert feature values to float (assuming numerical features)
-            feature1 = float(feature1)
-            feature2 = float(feature2)
-
-        # Perform prediction using the loaded model
-        prediction = model.predict([[feature1, feature2]])
-
-        # Return predictions as JSON response
+        # JSON response
         return jsonify({'prediction': prediction.tolist()})
 
     except Exception as e:
